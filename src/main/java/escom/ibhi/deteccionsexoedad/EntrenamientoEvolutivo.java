71c76d706b4df30e367e4bf52ed84f892d31d304
ivan <ivan@SUPER.ipe.com>
2015-07-08 21:06:46 -0500

    Inicializacion y documentacion de la clase EntrenamientoEvolutivo.java
    Faltan implementar los metodos

---
+++ src/main/java/escom/ibhi/deteccionsexoedad/EntrenamientoEvolutivo.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escom.ibhi.deteccionsexoedad;
+
+import org.encog.ml.MLMethod;
+import org.encog.ml.TrainingImplementationType;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.train.BasicTraining;
+import org.encog.neural.flat.FlatNetwork;
+import org.encog.neural.networks.ContainsFlat;
+import org.encog.neural.networks.training.Train;
+import org.encog.neural.networks.training.propagation.TrainingContinuation;
+
+/**
+ *
+ * @author ivan
+ */
+public class EntrenamientoEvolutivo extends BasicTraining implements Train{
+    
+    //Contiene la estructura de la red neuronal
+    private final FlatNetwork currentFlatNetwork;
+    
+    /*
+    *   @training : Contiene los valores de entrada y los valores de salida 
+    *               que se deberian de obtener
+    *   @network :  Contiene la estructura de la red neuronal
+    */
+    public EntrenamientoEvolutivo(ContainsFlat network, MLDataSet training) {
+        super(TrainingImplementationType.Iterative);
+        currentFlatNetwork = (FlatNetwork) network;
+        setTraining(training);
+        setIteration(0);
+        setError(0);
+        //Fantal especifiar las estrategias a utilizar
+    }
+    /*
+    *   Funcion que efecuta una iteración del entrenamiento de la red neuronal
+    */
+    @Override
+    public void iteration() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /*
+    *   Funcion para verificar si el entrenamiento convergio o si ocurrio algun
+    *   tipo de error
+    */
+    @Override
+    public boolean canContinue() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /*
+    *   Funcion que pausa al hijo que esta ejecutando el entrenamiento
+    */
+    @Override
+    public TrainingContinuation pause() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /*
+    *   Funcion que reanuda el entrenamiento despertando al hilo que se 
+    *   encarga del entrenamiento
+    */
+
+    @Override
+    public void resume(TrainingContinuation tc) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /*
+    *   Regresa la estrategia que se esta ejecutando en la iteracion presente,
+    *   el valor que tiene la variable iteration
+    *   Strategy, Se utilizan para saber si la red neuronal ha convergido, y
+    *   para saber el estado actual de la red neuronal, estras estrategias ya
+    *   estan implementadas solo hay que usarlas, hay demasiadas estrategias
+    *   que nos pueden ayudar realizar un mejor entrenamiento para ver todas las
+    *   estrategias ir a la pagina http://heatonresearch-site.s3-website-us-east-1.amazonaws.com/javadoc/encog-3.3/org/encog/ml/train/strategy/Strategy.html
+    */
+    @Override
+    public MLMethod getMethod() {
+        return (MLMethod) getStrategies().get(getIteration());
+    }
+}
